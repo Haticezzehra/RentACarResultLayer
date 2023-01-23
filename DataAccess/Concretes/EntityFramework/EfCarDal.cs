383679d1d6b2942fc1b436d0a1a1f1f19ad1d90a
example <haticezehraorhan@gmail.com>
2023-01-23 21:49:59 +0300

    EntityFramework

---
+++ DataAccess/Concretes/EntityFramework/EfCarDal.cs
@@ -0,0 +1,54 @@
+﻿using DataAccess.Abstract;
+using Entities.Concretes;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Concretes.EntityFramework
+{
+    public class EfCarDal : ICarDal
+    {
+        public void Add(Car car)
+        {
+            using (RentACarContext context = new RentACarContext())//Using kullanımı newlenen nesneyi kullanıktan sonr siler
+            {
+                var addEntity = context.Entry(car);
+                addEntity.State=EntityState.Added;
+            }
+        }
+
+        public void Delete(Car car)
+        {
+            using (RentACarContext context = new RentACarContext())
+            {
+                var deletedEntity = context.Entry(car);
+                deletedEntity.State = EntityState.Deleted;
+                context.SaveChanges();
+            }
+        }
+
+        public List<Car> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Car> GetAll(Expression<Func<Car, bool>> filter = null)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Car> GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(Car car)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ DataAccess/Concretes/EntityFramework/EfColorDal.cs
@@ -0,0 +1,39 @@
+﻿using DataAccess.Abstract;
+using Entities.Concretes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Concretes.EntityFramework
+{
+    public class EfColorDal : IColorDal
+    {
+        public void Add(Color color)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(Color color)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Color> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Color> GetAll(Expression<Func<Color, bool>> filter = null)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(Color color)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

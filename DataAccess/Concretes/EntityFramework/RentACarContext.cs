83dabe44ab3e520cc19a5062df171e80b357698e
example <haticezehraorhan@gmail.com>
2023-02-28 14:33:46 +0300

    CRUD

--- DataAccess/Concretes/EntityFramework/RentACarContext.cs
+++ DataAccess/Concretes/EntityFramework/RentACarContext.cs
@@ -19,5 +19,8 @@ namespace DataAccess.Concretes.EntityFramework
         public DbSet<Car> Car { get; set; }
         public DbSet<Brand> Brands { get; set; }
         public DbSet<Color> Color { get; set; }
+        public DbSet<User> Users { get; set; }
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<Rental> Rentals { get; set; }
     }
 }
